6f4ac9b8a492f15cdc3f4ddb0117039da72f7f3c
abhishek2316 <kumarabhi1623@gmail.com>
2025-10-20 23:57:23 +0530

    SetUp done for the Extension

---
+++ content.js
@@ -0,0 +1,20 @@
+console.log("Email Extension - Loaded content script.+");
+
+function injectButton() {}
+
+const observer = new MutationObserver((mutations) => {
+    for(const mutation of mutations) {
+        const addedNodes = Array.from(mutation.addedNodes);
+        const hasComposeElements = addedNodes.some(node =>
+            node.nodeType === Node.ELEMENT_NODE &&  
+            (node.matches('.aDh, .btC, [role="dialog"]') || node.querySelector('.aDh, .btC, [role="dialog"]'))
+        );
+
+        if(hasComposeElements) {
+            console.log("Compose Window has been detected!");
+            setTimeout(injectButton, 500);
+        }
+    }
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
